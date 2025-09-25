618d60342443c11ba73260bd3a1e68791676487c
hideops <128249753+hideops@users.noreply.github.com>
2025-09-25 02:18:26 -0700

    Update test_prog.cpp

--- tests/test_prog.cpp
+++ tests/test_prog.cpp
@@ -2,43 +2,46 @@
 #include <gtest/gtest.h>
 
 
-TEST(checkmatch, monday)
+TEST(chm, monday)
 {
     EXPECT_TRUE(cm("Понедельник", 12));
     EXPECT_FALSE(cm("Понедельник", 1));
     EXPECT_FALSE(cm("Понедельник", 154235));
 }
-TEST(checkmatch, tuesday)
+TEST(chm, tuesday)
 {
     EXPECT_TRUE(cm("Вторник", 100));
     EXPECT_TRUE(cm("Вторник", 15544));
     EXPECT_FALSE(cm("Вторник", 13));
 }
-TEST(checkmatch, wednesday)
+TEST(chm, wednesday)
 {
     EXPECT_TRUE(cm("Среда", 34));
     EXPECT_FALSE(cm("Среда", 14324));
 }
-TEST(checkmatch, thursday)
+TEST(chm, thursday)
 {
     EXPECT_TRUE(cm("Четверг", 0));
     EXPECT_FALSE(cm("Четверг", 866));
+    EXPECT_FALSE(cm("Четверг", 3261));
 }
-TEST(checkmatch, friday)
+TEST(chm, friday)
 {
     EXPECT_TRUE(cm("Пятница", 34));
-    EXPECT_TRUE(cm("Пятница", 2));
+    EXPECT_TRUE(cm("Пятница", 86));
     EXPECT_FALSE(cm("Пятница", 75));
     EXPECT_FALSE(cm("Пятница", 4737));
 }
-TEST(checkmatch, saturday)
+TEST(chm, saturday)
 {
     EXPECT_TRUE(cm("Суббота", 56));
     EXPECT_FALSE(cm("Суббота", 5475));
+    EXPECT_FALSE(cm("Суббота", 3411));
 }
-TEST(checkmatch, sunday)
+TEST(chm, sunday)
 {
     EXPECT_TRUE(cm("Воскресенье", 666));
     EXPECT_TRUE(cm("Воскресенье", -666));
     EXPECT_FALSE(cm("Воскресенье", 7437));
+    EXPECT_FALSE(cm("Воскресенье", 647181));
 }
