d59958e40648423f3781dac2a1f111771c7ea494
hideops <128249753+hideops@users.noreply.github.com>
2025-09-25 02:08:06 -0700

    Update test_prog.cpp

--- tests/test_prog.cpp
+++ tests/test_prog.cpp
@@ -4,37 +4,37 @@
 
 TEST(checkmatch, monday)
 {
-    EXPECT_TRUE(check_match("Понедельник", 12));
-    EXPECT_FALSE(check_match("Понедельник", 1));
+    EXPECT_TRUE(cm("Понедельник", 12));
+    EXPECT_FALSE(cm("Понедельник", 1));
 }
 TEST(checkmatch, tuesday)
 {
-    EXPECT_TRUE(check_match("Вторник", 100));;
-    EXPECT_FALSE(check_match("Вторник", 1));
+    EXPECT_TRUE(cm("Вторник", 100));;
+    EXPECT_FALSE(cm("Вторник", 1));
 }
 TEST(checkmatch, wednesday)
 {
-    EXPECT_TRUE(check_match("Среда", 34));
-    EXPECT_FALSE(check_match("Среда", 1));
+    EXPECT_TRUE(cm("Среда", 34));
+    EXPECT_FALSE(cm("Среда", 1));
 }
 TEST(checkmatch, thursday)
 {
-    EXPECT_TRUE(check_match("Четверг", 0));
-    EXPECT_FALSE(check_match("Четверг", 1));
+    EXPECT_TRUE(cm("Четверг", 0));
+    EXPECT_FALSE(cm("Четверг", 1));
 }
 TEST(checkmatch, friday)
 {
-    EXPECT_TRUE(check_match("Пятница", 12));
-    EXPECT_FALSE(check_match("Пятница", 1));
+    EXPECT_TRUE(cm("Пятница", 12));
+    EXPECT_FALSE(cm("Пятница", 1));
 }
 TEST(checkmatch, saturday)
 {
-    EXPECT_TRUE(check_match("Суббота", 56));
-    EXPECT_FALSE(check_match("Суббота", 1));
+    EXPECT_TRUE(cm("Суббота", 56));
+    EXPECT_FALSE(cm("Суббота", 1));
 }
 TEST(checkmatch, sunday)
 {
-    EXPECT_TRUE(check_match("Воскресенье", 666));
-    EXPECT_TRUE(check_match("Воскресенье", -666));
-    EXPECT_FALSE(check_match("Воскресенье", 1));
+    EXPECT_TRUE(cm("Воскресенье", 666));
+    EXPECT_TRUE(cm("Воскресенье", -666));
+    EXPECT_FALSE(cm("Воскресенье", 1));
 }
